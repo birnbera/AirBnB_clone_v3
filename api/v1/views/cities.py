ab5a04243a46b44eda5cb99aaec36ef321d38a9b
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-12-19 21:25:55 +0000

    Cities endpoint

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """City API endpoint"""
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
+from models.city import City
 
 @app_views.route('/cities', strict_slashes=False)
 def all_cities():
@@ -26,3 +27,33 @@ def delete_city(id):
         return jsonify({}), 200
     else:
         abort(404)
+
+@app_views.route('/cities', strict_slashes=False, methods=['POST'])
+def create_city():
+    """Create new City object from request JSON else raise 400"""
+    city = request.get_json()
+    if not city:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'name' not in city:
+        return jsonify({"error": "Missing name"}), 400
+    city = City(**city)
+    storage.new(city)
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
+def update_city(city_id):
+    """Update City object using data from JSON request else raise 400"""
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    updates = request.get_json()
+    if not updates:
+        return jsonify({"error": "Not a JSON"}), 400
+    updates.pop('id', None)
+    updates.pop('created_at', None)
+    updates.pop('updated_at', None)
+    for k,v  in updates.items():
+        setattr(city, k, v)
+    city.save()
+    return jsonify(city.to_dict()), 200
