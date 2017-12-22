5f8feb0bea25d70352ad57adb617be3bb74970a5
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-12-22 21:31:39 +0000

    Move app_views blueprint creation back into __init__.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """API endpoint"""
-from flask import jsonify, Blueprint
+from flask import jsonify
 from models import storage
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
-
+from api.v1.views import app_views
 
 @app_views.route('/status')
 def get_status():
